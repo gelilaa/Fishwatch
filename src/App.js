1b542acc56d43dcea1d77cb4047a12af557b39bb
gelilaa <gelila.ali@outlook.com>
2022-08-12 13:41:22 +0200

    time's up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React, {  useState ,useEffect} from "react";
 import "./App.css";
 import {Cards}from './components/Card.jsx'
-
-
+import { Button, Form} from "react-bootstrap";
 function App(){
   const [fish, setfishes] = useState([]);
+  const [input, SetInput] = useState();
 
+  const onChange =async (e)=>{
+     e.preventDefault()
+     try {
+      const data = await fetch(`https://www.fishwatch.gov/api/species`);
+      const dataJ = await data.json();
+       SetInput(dataJ)
+     
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  
  
   async function allFishes() {
     try {
       const data = await fetch(`https://www.fishwatch.gov/api/species`);
       const dataJ = await data.json();
-      console.log(dataJ);
-      return dataJ;
+      console.log(dataJ.species_name);
+    setfishes (dataJ);
     } catch (error) {
       console.error(error);
     }
   }
   useEffect(()=>{
   allFishes();
-})
+},[])
 
   return (
     <div className="App">
       FishWatch
-      <Cards/>
+      {fish.map((item, index) => {
+        return (
+          //<Cards key ={index} name= {item.speciesname} type={item.type}/>
+          console.log(item.Protein)
+        );
+      })}
+      <Form.Control
+        placeholder="Search City here..."
+        onChange={(e) => SetInput(e.target.value)}
+      />
+      <Button onClick={onChange}>search</Button>
+      {input}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-export const Cards =()=>{
+export const Cards =(name,type,onhandel)=>{
 return(
 <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          {type}
+        </Card.Text>
+        <Button variant="primary" onClick={onhandel}>read more</Button>
+      </Card.Body>
     </Card>
 )
 }
